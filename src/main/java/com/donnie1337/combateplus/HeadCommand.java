21d07079169a93e3a931d0b07a760ff7cd84875a
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-21 00:43:08 -0300

    fix: corrigir quebra de linha no HeadCommand

--- src/main/java/com/donnie1337/combateplus/HeadCommand.java
+++ src/main/java/com/donnie1337/combateplus/HeadCommand.java
@@ -31,7 +31,12 @@ public final class HeadCommand implements CommandExecutor {
             return true;
         }
 
-        if (!plugin.getConfig().getBoolean("cabecas.mobs-e-animais", true)) {\n            player.sendMessage(color("&c&lᴄᴏᴍʙᴀᴛᴇᴘʟᴜs &8• &cO sistema de cabeças de mobs e animais está desativado."));\n            return true;\n        }\n\n        ItemStack cabeca = plugin.getCabecasManager().criarCabecaPorNome(args[0]);
+        if (!plugin.getConfig().getBoolean("cabecas.mobs-e-animais", true)) {
+            player.sendMessage(color("&c&lᴄᴏᴍʙᴀᴛᴇᴘʟᴜs &8• &cO sistema de cabeças de mobs e animais está desativado."));
+            return true;
+        }
+
+        ItemStack cabeca = plugin.getCabecasManager().criarCabecaPorNome(args[0]);
         if (cabeca == null) {
             player.sendMessage(color("&c&lᴄᴏᴍʙᴀᴛᴇᴘʟᴜs &8• &cMob ou animal não encontrado."));
             return true;
