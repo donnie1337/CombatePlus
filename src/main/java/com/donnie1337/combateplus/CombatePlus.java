14a96a2ed185866c9f05e1fe4e14b1ff24ab4c80
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-22 18:47:11 -0300

    feat: adicionar comando pvp

--- src/main/java/com/donnie1337/combateplus/CombatePlus.java
+++ src/main/java/com/donnie1337/combateplus/CombatePlus.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CombatePlus extends JavaPlugin implements CommandExecutor {
@@ -31,6 +32,9 @@ public final class CombatePlus extends JavaPlugin implements CommandExecutor {
         if (getCommand("combateplus") != null) {
             getCommand("combateplus").setExecutor(this);
         }
+        if (getCommand("pvp") != null) {
+            getCommand("pvp").setExecutor(this);
+        }
         if (getCommand("head") != null) {
             getCommand("head").setExecutor(new HeadCommand(this));
         }
@@ -47,6 +51,17 @@ public final class CombatePlus extends JavaPlugin implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("pvp")) {
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage(message("mensagens.apenas-jogadores"));
+                return true;
+            }
+
+            boolean enabled = pvpListener.togglePvp(player);
+            sender.sendMessage(message(enabled ? "mensagens.pvp-ativado" : "mensagens.pvp-desativado"));
+            return true;
+        }
+
         if (!sender.hasPermission("combateplus.admin")) {
             sender.sendMessage(message("mensagens.sem-permissao"));
             return true;
